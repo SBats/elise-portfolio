1442f38e31984964d6c213940de2172b1b9c7263
SBats <simon.bats12@gmail.com>
2016-01-09 18:37:10 +0100

    add parallax effect

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,16 @@
+'use strict';
 
+function computeBackgroundOffset (element) {
+    var scrollPosition = document.querySelector('body').scrollTop;
+    element.style.backgroundPositionY = '-' + scrollPosition / 40 + 'px';
+}
+
+function mainController () {
+    var backgroundElement = document.querySelector('.main-container');
+    computeBackgroundOffset(backgroundElement);
+    window.addEventListener("scroll", function () {
+        computeBackgroundOffset(backgroundElement);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", mainController);
